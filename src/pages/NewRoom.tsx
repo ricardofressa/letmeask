a242ddc6c68c20fd480dfc31ff5f6c72561455dd
Ricardo Cardoso <ricardo.cardoso@luizalabs.com>
2021-07-11 16:00:15 -0300

    feat: add firebase deploy

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -40,6 +40,7 @@ export function NewRoom() {
         <strong>Crie salas de Q&amp;A ao-vivo</strong>
         <p>Tire as dúvidas da sua audiência em tempo-real</p>
       </aside>
+      
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
